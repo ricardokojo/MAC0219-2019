00d22ecef7bd78a4a1544cde88864a3291645df2
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2019-03-31 02:17:07 -0300

    Add get_files() function
    Add get_files() function that recursively checks files inside the given
    directory. It populates a string vector - passed as argument - with each
    file path.
    
    The code was based on:
    
    [1] https://stackoverflow.com/questions/13129340/recursive-function-for-listing-all-files-in-sub-directories
    [2] https://stackoverflow.com/questions/10750057/how-to-print-out-the-contents-of-a-vector
    
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

--- ep1/pgrep.cpp
+++ ep1/pgrep.cpp
@@ -1,30 +1,55 @@
+#include <cstring>
+#include <dirent.h>
 #include <iostream>
 #include <pthread.h>
-
+#include <vector>
 using namespace std;
 
 void pgrep() {
-  cout << "Pão\n";
+    cout << "Pão\n";
+}
+
+void get_files(const string &path, vector<string> &files, const bool show_hidden = false){
+    DIR *dir;
+    struct dirent *epdf;
+    dir = opendir(path.c_str());
+
+    if (dir != NULL){
+        while ((epdf = readdir(dir)) != NULL){
+            if(show_hidden ? (epdf->d_type==DT_DIR && string(epdf->d_name) != ".." && string(epdf->d_name) != "." ) : (epdf->d_type==DT_DIR && strstr(epdf->d_name,"..") == NULL && strstr(epdf->d_name,".") == NULL ) ){
+                get_files(path+epdf->d_name+"/",files, show_hidden);
+            }
+            if(epdf->d_type==DT_REG){
+                files.push_back(path+epdf->d_name);
+            }
+        }
+    }
+    closedir(dir);
 }
 
 int main(int argc, char *argv[]) {
-  if (argc < 4) {
-    cout << "usage: pgrep <MAX_THREADS> <REGEX_PESQUISA> <CAMINHO_DO_DIRETORIO>\n";
-    return 1;
-  }
+    if (argc < 4) {
+        cout << "usage: pgrep <MAX_THREADS> <REGEX_PESQUISA> <CAMINHO_DO_DIRETORIO>\n";
+        return 1;
+    }
 
-  int MAX_THREADS;
-  string REGEX, PATH;
+    int MAX_THREADS;
+    char *REGEX, *PATH;
+    vector<string> v;
+ 
+    MAX_THREADS = atoi(argv[1]);
+    REGEX = argv[2];
+    PATH = argv[3];
 
-  MAX_THREADS = atoi(argv[1]);
-  REGEX = argv[2];
-  PATH = argv[3];
+    cout << "MAX_THREADS: " << MAX_THREADS << "\n";
+    cout << "REGEX: " << REGEX << "\n";
+    cout << "PATH: " << PATH << "\n";
+    
+    pgrep();
+    get_files(PATH, v, false);
 
-  cout << "MAX_THREADS: " << MAX_THREADS << "\n";
-  cout << "REGEX: " << REGEX << "\n";
-  cout << "PATH: " << PATH << "\n";
-
-  pgrep();
-
-  return 0;
+    for (std::vector<string>::const_iterator i = v.begin(); i != v.end(); ++i)
+        cout << *i << endl;
+    
+    return 0;
 }
