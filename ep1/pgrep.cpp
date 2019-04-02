2a65865848f4598bb4b0be7ad5d3396fa6aa5d0a
Felipe Serras <felipeserras@gmail.com>
2019-04-02 18:03:01 -0300

    Introduzi o struct que agrega os dados necessarios para a ffunção das trheds, adicionei criação e finalização apropriada das threds, adicionei uma função de threads que não faz o que deveria fazer, mas serve de prova de funcionalidade e prototipa as formas de acesso correto às estruturas de dados. resta agora adicionar o conteúdo correto pra função de thread.

--- ep1/pgrep.cpp
+++ ep1/pgrep.cpp
@@ -8,13 +8,34 @@ using namespace std;
 
 
 //Os Mutexes são decarados como variáveis globais de forma que eles fiquem acessives para todas as threads:
+
+
 pthread_mutex_t lock_indexes;
 pthread_mutex_t lock_cout;
+struct thr_data{
+    vector<int>* indexes_ptr;
+    vector<vector<string>*>* findings_ptr;
+    vector<string>* files_ptr;
+};
 
 void pgrep() {
     cout << "Pão\n";
 }
 
+void *thr_func(void* arg) {
+    thr_data* data= (struct thr_data*) arg;
+    pthread_mutex_lock(&lock_cout);
+    pthread_mutex_lock(&lock_indexes);
+    int work_index=data->indexes_ptr->at(0);
+    data->findings_ptr->at(0)->push_back("Wi");
+    cout <<  data->findings_ptr->at(0)->back() << endl;
+    cout << data->files_ptr->at(0) << endl;
+    cout << "Olá eu sou uma thread! Eu estou viva e em breve morrerei." << endl;
+    pthread_mutex_unlock(&lock_cout);
+    pthread_mutex_unlock(&lock_indexes);
+
+    pthread_exit(NULL);
+}
 
 //Falta adicionar um modo recursivo:
 void get_files(const string &path, vector<string> &files, const bool show_hidden = false){
@@ -66,6 +87,31 @@ int main(int argc, char *argv[]) {
     }
     pthread_mutex_init(&lock_indexes,NULL);
     pthread_mutex_init(&lock_cout,NULL);
+
+    pthread_t thr[MAX_THREADS];
+    int thr_data=0;
+    int rc;
+
+    if (files.size()<MAX_THREADS){
+        MAX_THREADS=files.size();}
+
+
+
+    struct thr_data data;
+    data.indexes_ptr=&indexes;
+    data.findings_ptr=&findings;
+    data.files_ptr=&files;
+
+    for(int i=0;i<MAX_THREADS;i++){
+        if ((rc = pthread_create(&thr[i], NULL, thr_func, &data))) {
+            fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
+            return EXIT_FAILURE;
+        }
+    } 
+
+    for(int i=0;i<MAX_THREADS;i++){
+        pthread_join(thr[i],NULL);
+    }
     
     // string line="oi";
     // findings.at(0)->push_back(line);
