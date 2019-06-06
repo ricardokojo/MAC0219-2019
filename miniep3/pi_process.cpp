a3ecf912f67024d805802fc858d600900176f957
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2019-06-06 10:21:11 -0300

    Complete MiniEP3 pi_process.cpp
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

--- miniep3/pi_process.cpp
+++ miniep3/pi_process.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 #include <math.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 unsigned NUM_PROCESSOS; // number of worker threads
 unsigned NUM_PONTOS;    // number of divisions at integration
-double pi_by_4 = 0;     // approximation of pi/4
+double *pi_by_4;        // approximation of pi/4
 
 struct task
 {
   int start, end;
 };
 
-void *process_word(void *arg)
+#define DIE(...)                  \
+  {                               \
+    fprintf(stderr, __VA_ARGS__); \
+    exit(EXIT_FAILURE);           \
+  }
+
+void *process_work(void *arg)
 {
   struct task *t = (struct task *)arg;
-  double acc = 0;                 // Thread's local integration variable
-  double interval_size = 1.0 / N; // The circle radius is 1.0
+  double acc = 0;                          // Thread's local integration variable
+  double interval_size = 1.0 / NUM_PONTOS; // The circle radius is 1.0
 
   // Integrates f(x) = sqrt(1 - x^2) in [t->start, t->end[
   for (int i = t->start; i < t->end; ++i)
@@ -23,18 +34,18 @@ void *process_word(void *arg)
     acc += sqrt(1 - (x * x)) * interval_size;
   }
 
-  // This is a critical section. As we are going to write to a global
-  // value, the operation must me protected against race conditions.
-  // pthread_mutex_lock(&lock);
-  pi_by_4 += acc;
-  // pthread_mutex_unlock(&lock);
+  // std::cout << acc << "\n";
+  pi_by_4[0] += acc;
 
-  return NULL;
+  exit(0);
 }
 
 int main(int argc, char const *argv[])
 {
   struct task *tasks;
+  pid_t pid;
+  pid_t *list_of_pids;
+  int status;
 
   if (argc != 3)
   {
@@ -45,37 +56,55 @@ int main(int argc, char const *argv[])
   NUM_PROCESSOS = atoi(argv[1]);
   NUM_PONTOS = atoi(argv[2]);
 
-  std::cout << NUM_PROCESSOS << " " << NUM_PONTOS << "\n";
-
-  if ((tasks = std::malloc(NUM_PROCESSOS * sizeof(struct task))) == NULL)
+  // Malloc tasks
+  if ((tasks = (task *)std::malloc(NUM_PROCESSOS * sizeof(struct task))) == NULL)
     DIE("Tasks malloc failed\n");
+  if ((list_of_pids = (pid_t *)std::malloc(NUM_PROCESSOS * sizeof(pid_t))) == NULL)
+    DIE("list_of_pids malloc failed\n");
 
-  // Initialize threads with default attributes.
-  // The work is being splitted as evenly as possible between threads.
-  int threads_with_one_more_work = NUM_PONTOS % NUM_PROCESSOS;
+  // Separate tasks
+  int processes_with_one_more_work = NUM_PONTOS % NUM_PROCESSOS;
   for (int i = 0; i < NUM_PROCESSOS; ++i)
   {
     int work_size = NUM_PONTOS / NUM_PROCESSOS;
-    if (i < threads_with_one_more_work)
+    if (i < processes_with_one_more_work)
       work_size += 1;
     tasks[i].start = i * work_size;
     tasks[i].end = (i + 1) * work_size;
-
-    // fork()
-
-    // if (pthread_create(&threads[i], NULL, thread_work, (void *)&tasks[i]))
-    // DIE("Failed to create thread %d\n", i)
   }
 
-  // Finish threads and ignore their return values
-  for (int i = 0; i < num_threads; ++i)
+  // Create mmap and call forks()
+  pi_by_4 = (double *)mmap(NULL, sizeof(double), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+
+  for (int i = 0; i < NUM_PROCESSOS; ++i)
   {
-    if (pthread_join(threads[i], NULL))
-      DIE("failed to join thread %d\n", i);
+    pid = fork();
+    if (pid == 0) // child
+    {
+      process_work((void *)&tasks[i]);
+    }
+    else if (pid > 0) // parent
+    {
+      list_of_pids[i] = pid;
+    }
+    else // error -> clear memory
+    {
+      free(tasks);
+      free(list_of_pids);
+      munmap(pi_by_4, sizeof(double));
+      DIE("Fork failed\n");
+    }
   }
 
-  printf("pi ~= %.12f\n", pi_by_4 * 4);
+  // Call waitpid() for each PID
+  for (int i = 0; i < NUM_PROCESSOS; ++i)
+  {
+    waitpid(pid, &status, WUNTRACED);
+  }
+
+  printf("%.12f\n", pi_by_4[0] * 4);
 
   free(tasks);
+  munmap(pi_by_4, sizeof(double));
   return 0;
 }
