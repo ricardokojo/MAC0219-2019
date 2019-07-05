47bc55dc214e25858a842ae2a4d5df2963ef0134
Ricardo H H Kojo <rkojo@conceptainvestimentos.com.br>
2019-07-05 19:31:36 -0300

    Revert changes
    Signed-off-by: Ricardo H H Kojo <rkojo@conceptainvestimentos.com.br>

--- miniep4/gpu_work_v2.cu
+++ miniep4/gpu_work_v2.cu
@@ -1,80 +1,47 @@
 #include "utils.h"
-#include <assert.h>
 
 #define THS_PER_BLOCK 256
-#define UNTIL_NORM 100
 
 __global__
-void gpu_work_le(double *arr, bool *is_less)
+void gpu_work_v2(double *arr)
 {
-    const int id = blockDim.x * blockIdx.x + threadIdx.x;
+        const int id = blockDim.x * blockIdx.x + threadIdx.x;
 
-    if (id >= ARR_SIZE || !is_less[id]) return;
+        if (id >= ARR_SIZE) return;
 
-    for(int i = 0; i < (GPU_WORK_ITERATIONS-UNTIL_NORM); ++i) {
-        if (arr[id] <= 0.5)
-            arr[id] = laborious_func_le_half(arr[id]);
-        else
-            arr[id] = laborious_func_gt_half(arr[id]);
-    }
-}
+        int delayed = 0;
+        int cond;
 
-__global__
-void gpu_work_gt(double *arr, bool *is_less)
-{
-    const int real_id = blockDim.x * blockIdx.x + threadIdx.x;
-    const int id = real_id % ARR_SIZE;
+        for(int i = 0; i < GPU_WORK_ITERATIONS;) {
+                if (!delayed)
+                        cond = (arr[id] <= 0.5);
 
-    if (real_id >= ARR_SIZE && is_less[id]) return;
-    if (real_id < ARR_SIZE && !is_less[id]) return;
-
-    for(int i = UNTIL_NORM; i < GPU_WORK_ITERATIONS; ++i) {
-        if (arr[id] <= 0.5)
-            arr[id] = laborious_func_le_half(arr[id]);
-        else
-            arr[id] = laborious_func_gt_half(arr[id]);
-    }
-}
-
-__global__
-void gpu_work_stable(double *arr, bool *is_less)
-{
-    const int id = blockDim.x * blockIdx.x + threadIdx.x;
-
-    if (id >= ARR_SIZE) return;
-
-    for(int i = 0; i < GPU_WORK_ITERATIONS && i < UNTIL_NORM; ++i) {
-        if (arr[id] <= 0.5)
-            arr[id] = laborious_func_le_half(arr[id]);
-        else
-            arr[id] = laborious_func_gt_half(arr[id]);
-    }
-
-    is_less[id] = (arr[id] <= 0.5);
+                int cond_for_all = !cond_for_all;
+                delayed = (cond_for_all != cond);
+                if (!delayed) {
+                        if (cond_for_all) {
+                                arr[id] = laborious_func_le_half(arr[id]);
+                        } else {
+                                arr[id] = laborious_func_gt_half(arr[id]);
+                        }
+                        ++i;
+                }
+        }
 }
 
 // Launch the work on arr and return it at results;
 void launch_gpu_work_v2(double *arr, double **results)
 {
-    double *d_arr;
-    bool *d_is_less;
+        double *d_arr;
 
-    cudaAssert(cudaMalloc(&d_arr, ARR_SIZE * sizeof(double)));
-    cudaAssert(cudaMalloc(&d_is_less, ARR_SIZE * sizeof(bool)));
+        cudaAssert(cudaMalloc(&d_arr, ARR_SIZE * sizeof(double)));
+        cudaAssert(cudaMemcpy(d_arr, arr, ARR_SIZE * sizeof(double),
+                              cudaMemcpyHostToDevice));
 
-    cudaAssert(cudaMemcpy(d_arr, arr, ARR_SIZE * sizeof(double),
-                          cudaMemcpyHostToDevice));
+        gpu_work_v2<<<DIV_CEIL_INT(ARR_SIZE, THS_PER_BLOCK), THS_PER_BLOCK>>>(d_arr);
+        cudaAssert(cudaDeviceSynchronize());
 
-    gpu_work_stable<<<DIV_CEIL_INT(ARR_SIZE, THS_PER_BLOCK), THS_PER_BLOCK>>>(d_arr, d_is_less);
-    cudaAssert(cudaDeviceSynchronize());
-
-    // gpu_work_le<<<DIV_CEIL_INT(ARR_SIZE, THS_PER_BLOCK), THS_PER_BLOCK>>>(d_arr, d_is_less);
-    gpu_work_gt<<<DIV_CEIL_INT(ARR_SIZE, THS_PER_BLOCK)*2, THS_PER_BLOCK>>>(d_arr, d_is_less);
-    cudaAssert(cudaDeviceSynchronize());
-
-    cudaAssert(cudaMemcpy(*results, d_arr, ARR_SIZE * sizeof(double),
-                          cudaMemcpyDeviceToHost));
-
-    cudaAssert(cudaFree(d_arr));
-    cudaAssert(cudaFree(d_is_less));
+        cudaAssert(cudaMemcpy(*results, d_arr, ARR_SIZE * sizeof(double),
+                              cudaMemcpyDeviceToHost));
+        cudaAssert(cudaFree(d_arr));
 }
