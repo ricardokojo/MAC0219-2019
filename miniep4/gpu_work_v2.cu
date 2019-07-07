bd393d81948230dd5bdf1b9fcdc1277440608ada
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2019-07-07 16:39:05 -0300

    Add solution for MiniEP4
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

--- miniep4/gpu_work_v2.cu
+++ miniep4/gpu_work_v2.cu
@@ -2,49 +2,54 @@
 
 #define THS_PER_BLOCK 256
 
-__global__
-void gpu_work_v2(double *arr)
+__global__ void gpu_work_v2(double *arr)
 {
-        const int id = blockDim.x * blockIdx.x + threadIdx.x;
+    const int id = blockDim.x * blockIdx.x + threadIdx.x;
 
-        if (id >= ARR_SIZE) return;
+    if (id >= ARR_SIZE)
+        return;
 
-        int delayed = 0;
-        int cond;
-        double a = arr[id];
+    int delayed = 0;
+    int cond;
+    double a = arr[id];
 
-        for(int i = 0; i < GPU_WORK_ITERATIONS;) {
-                if (!delayed)
-                        cond = (a <= 0.5);
+    for (int i = 0; i < GPU_WORK_ITERATIONS;)
+    {
+        if (!delayed)
+            cond = (a <= 0.5);
 
-                int cond_for_all = !cond_for_all;
-                delayed = (cond_for_all != cond);
-                if (!delayed) {
-                        if (cond_for_all) {
-                                a = laborious_func_le_half(a);
-                        } else {
-                                a = laborious_func_gt_half(a);
-                        }
-                        ++i;
-                }
+        int cond_for_all = !cond_for_all;
+        delayed = (cond_for_all != cond);
+        if (!delayed)
+        {
+            if (cond_for_all)
+            {
+                a = laborious_func_le_half(a);
+            }
+            else
+            {
+                a = laborious_func_gt_half(a);
+            }
+            ++i;
         }
+    }
 
-        arr[id] = a;
+    arr[id] = a;
 }
 
 // Launch the work on arr and return it at results;
 void launch_gpu_work_v2(double *arr, double **results)
 {
-        double *d_arr;
+    double *d_arr;
 
-        cudaAssert(cudaMalloc(&d_arr, ARR_SIZE * sizeof(double)));
-        cudaAssert(cudaMemcpy(d_arr, arr, ARR_SIZE * sizeof(double),
-                              cudaMemcpyHostToDevice));
+    cudaAssert(cudaMalloc(&d_arr, ARR_SIZE * sizeof(double)));
+    cudaAssert(cudaMemcpy(d_arr, arr, ARR_SIZE * sizeof(double),
+                          cudaMemcpyHostToDevice));
 
-        gpu_work_v2<<<DIV_CEIL_INT(ARR_SIZE, THS_PER_BLOCK), THS_PER_BLOCK>>>(d_arr);
-        cudaAssert(cudaDeviceSynchronize());
+    gpu_work_v2<<<DIV_CEIL_INT(ARR_SIZE, THS_PER_BLOCK), THS_PER_BLOCK>>>(d_arr);
+    cudaAssert(cudaDeviceSynchronize());
 
-        cudaAssert(cudaMemcpy(*results, d_arr, ARR_SIZE * sizeof(double),
-                              cudaMemcpyDeviceToHost));
-        cudaAssert(cudaFree(d_arr));
+    cudaAssert(cudaMemcpy(*results, d_arr, ARR_SIZE * sizeof(double),
+                          cudaMemcpyDeviceToHost));
+    cudaAssert(cudaFree(d_arr));
 }
